9801a1afd53cf9f8f403d16634027b225bdc492f
avazquezok <avazquezrochaok@gmail.com>
2023-03-28 10:34:01 -0300

    new_start

--- script.js
+++
@@ -1,7 +0,0 @@
-function hamburger() {
-  let menubar = document.getElementById("myLinks");
-if (menubar.style.display === "block") {
-  menubar.style.display = "none";
-} else {
-  menubar.style.display = "block";
-}}
